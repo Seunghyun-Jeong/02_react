7181ce86470d06703d87d6ecf33e069c68f695c8
YeonjiKim0316 <kimyeonji3@gmail.com>
2026-09-07 15:10:52 +0900

    fix: 계좌번호 보기 버튼을 state로 변경하여 AccountCard 컴포넌트에 prop으로 사용

--- won-banking-mini/src/App.jsx
+++ won-banking-mini/src/App.jsx
@@ -4,7 +4,9 @@ import Clock from './components/Clock.jsx'
 import Panel from './components/Panel.jsx'
 import AccountCard  from './components/AccountCard.jsx'
 import Header from './components/Header'
-
+import Counter from './components/Counter.jsx'
+import { useState } from 'react'
+ 
 // 02_html기초.html 안에 만들었던 계좌카드의 css를 가져와서
 // 아래에 있는 카드를 좀더 그럴듯하게 꾸며보세요.
 // 실제로 사용될 화면을 그립니다.
@@ -42,7 +44,8 @@ function App() {
 
   // flag 변수: 깃발을 들어서 교통량을 제어하는 것처럼 이 변수의 역할은 특정 로직을 끄거나 켜거나 밖에 없기 때문에
   // flag 변수를 사용할 때는 default 값을 false로 만들고 시작하는 로직을 권장 
-  let showFullNo = true
+  const [showFullNo, setShowFullNo] = useState(false);
+  //     ↑현재 값      ↑바꾸는 함수              ↑처음값
   
   // XML에서는 여는 꺽쇠 안의 태그가 무엇이든 될 수 있기 때문에 <이름>김연지 </이름>
   // JSX 가 소문자 태그는 HTML, 대문자로 시작하는 태그는 컴포넌트로 인식
@@ -51,6 +54,11 @@ function App() {
   return (
     <> 
     <Header />
+
+    <button onClick={() => setShowFullNo(!showFullNo)}>
+      {showFullNo ? "계좌번호 숨기기" : "계좌번호 보기"}
+    </button>
+    {console.log(showFullNo)} 
     <Clock />
     {/* class 는 JS의 예약어이므로 JSX에서는 className으로 대신 사용합니다.*/}
 
@@ -59,13 +67,15 @@ function App() {
       <AccountCard accountNo={accounts[0].accountNo}
                   accountType={accounts[0].accountType} 
                   balance={accounts[0].balance}
-                  status={accounts[0].status}/>
+                  status={accounts[0].status}
+                  showFullNo={showFullNo} />
       {/* 두번째 AccountCard가 출력되도록 accounts[1] dict의 값과 매핑해주세요. */}
     
       <AccountCard accountNo={accounts[1].accountNo}
                   accountType={accounts[1].accountType} 
                   balance={accounts[1].balance}
-                  status={accounts[1].status}/>
+                  status={accounts[1].status}
+                  showFullNo={showFullNo} />
     
     </Panel>
     </>
--- won-banking-mini/src/components/AccountCard.jsx
+++ won-banking-mini/src/components/AccountCard.jsx
@@ -2,14 +2,14 @@
 import StatusBadge from "./StatusBadge";
 import { formatWon, maskAccountNo } from "../utils/format";
 
-function AccountCard({ accountNo, accountType, balance, status }) {
+function AccountCard({ accountNo, accountType, balance, status, showFullNo }) {
   return (
     <div className="card">
       <div className="row">
         <span className="muted">{accountType}</span>
         <StatusBadge status={status} />
       </div>
-      <p className="muted">{maskAccountNo(accountNo)}</p>
+      <p className="muted">{showFullNo ? accountNo : maskAccountNo(accountNo)}</p>
       <strong className="balance">{formatWon(balance)}</strong>
     </div>
   );
